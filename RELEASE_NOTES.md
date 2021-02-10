64a07e7384139a8c3a29e0e4c67539c223b6be71
Christian Luksch <luksch@vrvis.at>
2021-02-10 23:44:33 +0100

    updated build script; added RELEASE_NOTES.md;

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+### 0.2.0
+
+- updated to Open Image Denoise 1.3.0
+- updated Aardvark to 5.1
+
+### 0.1.0
+
+- initial
--- build.fsx
+++ build.fsx
@@ -6,6 +6,9 @@ open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.IO.Globbing.Operators
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
@@ -15,6 +18,12 @@ DefaultSetup.install ["Aardvark.OpenImageDenoise.sln"]
 #if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
 #endif
+
+Target.create "PushDev" (fun _ -> 
+    DefaultSetup.push ["https://vrvis.myget.org/F/aardvark_public/api/v2" ,"public.key"]
+)
+
+"CreatePackage" ==> "PushDev"
 
 
 entry()
